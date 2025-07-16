3663891273269cf7e969ac8757abcd9f0d70b3cc
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-07-15 21:29:38 -0400

    basics

--- Course01/PrimitiveDataTypes/src/App.java
+++ Course01/PrimitiveDataTypes/src/App.java
@@ -1,5 +1,20 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) 
+    {
+        int width;
+        int height;
+        double radius = 12.5;
+        boolean result = true;
+
+        width = 50;
+
+        System.out.println("test");
+
+        System.out.println("The value of width is " + width);
+
+        int[] daysInMonth = new int[3];
+        String[] monthNames = { "One", "Two", "Three" };
+
+
     }
 }
