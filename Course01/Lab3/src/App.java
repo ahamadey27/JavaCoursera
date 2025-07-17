c3233503155573acac3875ba93fd5f95034e4250
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-07-17 12:05:19 -0400

    Add functionality to print days of the week

--- Course01/Lab3/src/App.java
+++ Course01/Lab3/src/App.java
@@ -1,6 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception 
     {
+        String[] daysOfWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+        for (String dayOfWeek : daysOfWeek) 
+        {
+            System.out.println(dayOfWeek.toString());
+            
+        }
+
         
+        
+
     }
 }
