1009e458723d94988866cbbc8ad548c6d0f7d0c2
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-07-17 12:13:40 -0400

    reversed array

--- Course01/Lab3/src/App.java
+++ Course01/Lab3/src/App.java
@@ -3,11 +3,20 @@ public class App {
     {
         String[] daysOfWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
 
+        //Print days in order
         for (String dayOfWeek : daysOfWeek) 
         {
             System.out.println(dayOfWeek.toString());
-            
         }
+
+        System.out.println();
+
+        //Print days in reverse 
+        for (int i = 0; i < daysOfWeek.length; i++)
+        {
+            System.out.println(daysOfWeek[i]);
+        }
+        
 
         
         
