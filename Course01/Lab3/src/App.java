42358d88f2558d5a7d71ac739bc049ccbe0c7934
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-07-17 15:09:32 -0400

    done for the day

--- Course01/Lab3/src/App.java
+++ Course01/Lab3/src/App.java
@@ -36,11 +36,62 @@ public class App {
         // Create a for loop that will print out all numbers between 1 and 100 except 50 and 60
         for (int i = 1; i <= 100; i++)
         {
-            if (i >= 50 && i <= 60)
-            {
+            if (i >= 50 && i <= 60) {
                 continue;
             }
             System.out.println(i);
+        }
+        
+        System.out.println();
+        System.out.println();
+        
+        //Switch statement exercise
+        int counter = 1;
+        while (counter <= 12)
+        {
+            switch(counter)
+            {
+                case 1:
+                    System.out.println("January has 31 days");
+                    break;
+                case 2:
+                    System.out.println("February has 28 days");
+                    break;
+                case 3:
+                    System.out.println("March has 31 days");
+                    break;
+                case 4:
+                    System.out.println("April has 30 days");
+                    break;
+                case 5:
+                    System.out.println("May has 31 days");
+                    break;
+                case 6:
+                    System.out.println("June has 30 days");
+                    break;
+                case 7:
+                    System.out.println("July has 31 days");
+                    break;
+                case 8:
+                    System.out.println("August has 31 days");
+                    break;
+                case 9:
+                    System.out.println("September has 30 days");
+                    break;
+                case 10:
+                    System.out.println("October has 31 days");
+                    break;
+                case 11:
+                    System.out.println("November has 30 days");
+                    break;
+                case 12:
+                    System.out.println("December has 31 days");
+                    break;
+                default:
+                    System.out.println("Invalid month");
+                    break;
+            }
+            counter++;
         }
         
     }
